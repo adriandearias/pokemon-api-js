6a606c2e3fbd7d15f74a17870a816cc3f0e5b294
adriandearias <deariasadrian@gmail.com>
2023-04-22 12:01:23 +0200

    added loading while waiting fetch

--- api.js
+++ api.js
@@ -1,11 +1,14 @@
 //get 151 Pokemons(first generation)
 export async function getPokemons(){
     const arrayPokemons = []
+    const loadingElement = document.getElementById('loading');
+    loadingElement.style.display = 'block';
     for (let i = 1; i <= 151; i++) {
         const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         const pokemonResultToObject = await result.json()   
         arrayPokemons.push(pokemonResultToObject)  
     }
+    loadingElement.style.display = 'none'
     return arrayPokemons
 }
 
